7a2b97ec75a2a4d53c9455cc0b9fcd73fd41195a
Walter Da Col <walter.dacol@gmail.com>
2010-11-26 17:21:37 +0100

    Renamed file and fix Issue #2
    *nautilus-gloobus-preview.py: Implemented hotkey in this file, next
    	commit will add library and user app for changing hotkey.
    	Minor string changes

--- nautilus-gloobus-preview.py
+++ nautilus-gloobus-preview.py
@@ -2,7 +2,7 @@
 
 # This file is part of Nautilus Gloobus Preview Extension
 
-# Copyright (c) 2009-2010 - Walter Da Col <walter.dacol@gmail.com>
+# Copyright (c) 2010-2011 - Walter Da Col <walter.dacol@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -21,11 +21,21 @@ import nautilus
 import gobject
 import subprocess
 import urllib
-import locale
 import gtk
 import os
-# Localization
-from gettext import gettext as _
+# i18n
+import gettext
+
+try:
+	gettext.install('nautilus-gloobus-preview', unicode=1)
+	gettext.textdomain('nautilus-gloobus-preview')
+except:
+	pass
+
+_ = gettext.gettext
+
+# Key mask
+from gp_hotkey.libkeymask import *
 
 
 def run_cmd(cmd):
@@ -43,11 +53,43 @@ def run_cmd(cmd):
 class GloobusPreviewExtension(nautilus.MenuProvider):
 	
 	VER = '0.1.0'
-	APP = 'nautilus-gloobus'
+	APP = 'nautilus-gloobus-preview'
 	MSG_BASE = '['+APP+']'
 	CURRENT_FILE = ''
 	CURRENT_WINDOW = None
-
+	USER_FILE = ''
+	USER_MOD = []
+	USER_KEY = gtk.gdk.keyval_from_name('space')
+	
+	def get_saved_hotkey(self):
+		"""
+		Load stored hotkey from user, fallback to default (space) if not found
+		"""
+		
+		self.USER_FILE = os.path.expanduser('~/.config/nautilus-gloobus-preview/hotkey.cfg')
+		# Check if file exists
+		if not os.path.exists(self.USER_FILE):
+			MSG = self.MSG_BASE + _('User\'s keybind not found at %s, reverting to \'space\'') % self.USER_FILE
+			print MSG
+			return False
+		# Open file and reading string
+		try:
+			f = open(self.USER_FILE,'r')
+			accel_string = f.read()
+			f.close()
+		except Exception:
+			MSG = self.MSG_BASE + _('Error in user\'s file %s') % self.USER_FILE
+			print MSG
+			return False
+		# Validate string
+		if not is_accel_string_valid(accel_string):
+			MSG = self.MSG_BASE + _('User\'s file %s not valid') % self.USER_FILE
+			print MSG
+			return False
+		# Setting up variables
+		self.USER_MOD = get_mod_list_from_accel_string(accel_string)
+		self.USER_KEY = gtk.gdk.keyval_from_name(get_key_from_accel_string(accel_string))
+		return True
 
 	def can_preview(self, widget):
 		"""
@@ -67,9 +109,9 @@ class GloobusPreviewExtension(nautilus.MenuProvider):
 		return (iconview or treeview or deskview)
 	
 	def on_key_press_event(self, widget, event):
+		
 		#Check if key pressed is my hotkey
-		#TODO: implement editable hotkeys
-		if event.keyval == gtk.gdk.keyval_from_name('space'):
+		if (get_mod_list_from_event(event) == self.USER_MOD) & (event.keyval == self.USER_KEY):
 			#Check if i'm on an Icon (and not editing it)
 			if self.can_preview(widget):
 				#Blocking default space Action (Open File)
@@ -81,6 +123,9 @@ class GloobusPreviewExtension(nautilus.MenuProvider):
 	def __init__(self):
 		MSG = _('Initializing %s extension') % self.APP
 		print MSG
+		self.get_saved_hotkey()
+		
+		
 	
 	def menu_activate_cb(self, menu, file):
 		"""
